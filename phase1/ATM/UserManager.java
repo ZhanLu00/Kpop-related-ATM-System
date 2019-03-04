56cc79e24fea7518d9ca494d660152050c32e9ad
ahnuy <yuhan25@hotmail.com>
2019-03-04 18:41:22 -0500

    addUser, removeUser, userExists methods added

--- phase1/ATM/UserManager.java
+++ phase1/ATM/UserManager.java
@@ -1,4 +1,32 @@
 package ATM;
 
+import java.util.ArrayList;
+
 public class UserManager {
+
+    public ArrayList <User> users;
+
+    public UserManager(){
+        this.users = new ArrayList<User>;
+    }
+
+    public void addUser(User user){
+        users.add(user);
+    }
+
+    public void removeUser(User user){
+        users.remove(user);
+    }
+
+    public boolean userExists(String username, String password){
+        for (User u : users){
+            if (u.getUsername().equals(username)){
+                if (u.getPassword().equals(password)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }

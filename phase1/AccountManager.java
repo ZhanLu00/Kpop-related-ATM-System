482767ff3ce6e712fcf3428cba6665faf80a3c70
KarenChen <kareny.chen@mail.utoronto.ca>
2019-03-04 15:52:42 -0500

    changes on AccountManager

--- phase1/AccountManager.java
+++ phase1/AccountManager.java
@@ -1,2 +1,14 @@
+import BankAccounts.BankAccount;
+
+import java.util.ArrayList;
+
 public class AccountManager {
+    public ArrayList<BankAccount> accounts;
+
+    public AccountManager(){
+        this.accounts = new ArrayList<BankAccount>();
+    }
+
+
+
 }

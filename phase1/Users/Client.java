2c1c7f32bc4da2050b7c267bbeeb996e33b1ad32
Zhan Lu <zhanlu0523@gmail.com>
2019-03-04 15:55:05 -0500

    modify user class

--- phase1/Users/Client.java
+++ phase1/Users/Client.java
@@ -1,11 +1,35 @@
 package Users;
 
+
 public class Client extends User {
 
     private static String createdDate;
 
-    public Client(String username, String password) {
+    public Client(String username, String createdDate, String password) {
         super(username, password);
+        this.createdDate = createdDate;
     }
+
+    // stores
+
+
+
+    // getters
+    public String getCreatedDate(){
+        return this.createdDate;
+    }
+
+    public String getUsername(){
+        return this.password;
+    }
+
+    public String getPassword(){
+        return this.UserNae
+    }
+
+    // setters
+
+
+
 
 }

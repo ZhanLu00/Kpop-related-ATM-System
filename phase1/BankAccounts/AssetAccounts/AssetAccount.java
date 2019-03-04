123245b5f8509b5fb6b2619326cff057f225cc0a
KarenChen <kareny.chen@mail.utoronto.ca>
2019-03-04 13:31:07 -0500

    constructed subclasses of BankAccount with basic functionality

--- phase1/BankAccounts/AssetAccounts/AssetAccount.java
+++ phase1/BankAccounts/AssetAccounts/AssetAccount.java
@@ -3,7 +3,11 @@ package BankAccounts.AssetAccounts;
 import BankAccounts.BankAccount;
 
 public abstract class AssetAccount extends BankAccount {
-    protected AssetAccount(int id) {
-        super(id);
+    AssetAccount(String password, Users.Client client) {
+        super(password, client);
+    }
+
+    public void deposit(double amount) {
+        this.balance -= amount;
     }
 }

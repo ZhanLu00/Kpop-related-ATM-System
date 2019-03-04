5257c664d68db5b40a9a37373440a28641ea28b5
joyce-ma <joycema.ma@mail.utoronto.ca>
2019-03-04 16:39:49 -0500

    getter/setters for Atm class

--- phase1/ATM/Atm.java
+++ phase1/ATM/Atm.java
@@ -4,6 +4,28 @@ public class Atm {
     UserManager userManager;
     BankManager bankManager;
     TimeManager timeManager;
+    ActionManager actionManager;
+    public Atm (UserManager userManager, BankManager bankManager, TimeManager timeManager
+    ActionManager actionManager){
+        this.userManager = userManager;
+        this.bankManager = bankManager;
+        this.timeManager = timeManager;
+        this.actionManager = actionManager;
+    }
+    public User getUser(String username, String password){
+        return userManager.getUser(username, password);
+    }
+    public void setUser(User user){
+        userManager.addUser(user);
+    }
+
+    public BankManager getBankManager() {
+        return bankManager;
+    }
+
+    public void setBankManager(bankManager bankManager){
+        this.bankManager = bankManager;
+    }
 
 
 }

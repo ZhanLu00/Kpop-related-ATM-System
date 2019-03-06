a373f3ee79753be986ac7e72600e9f5520d699fe
ahnuy <yuhan25@hotmail.com>
2019-03-06 12:43:55 -0500

    initializer & getters for BillManager

--- phase1/ATM/BillManager.java
+++ phase1/ATM/BillManager.java
@@ -1,4 +1,36 @@
 package ATM;
 
 public class BillManager {
+    public int fives;
+    public int tens;
+    public int twenties;
+    public int fifties;
+
+    public BillManager(){
+        this.fives = 0;
+        this.tens = 0;
+        this.twenties = 0;
+        this.fifties = 0;
+    }
+
+    public BillManager(int fives, int tens, int twenties, int fifties){
+        this.fives = fives;
+        this.tens = tens;
+        this.twenties = twenties;
+        this.fifties = fifties;
+    }
+
+    public int getFives(){
+        return this.fives;
+    }
+    public int getTens(){
+        return this.tens;
+    }
+    public int getTwenties(){
+        return this.twenties;
+    }
+    public int getFifties(){
+        return this.fifties;
+    }
+
 }

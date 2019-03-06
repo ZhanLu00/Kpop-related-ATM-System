08a8fe3ae3a1c7ae6f27f5d0fb7ac7d098486f71
SeelPatel <seelpatel@gmail.com>
2019-03-05 23:03:12 -0500

    Reimplemented TimeManager and Added readers and writers for other atm info.

--- phase1/ATM/test.java
+++ phase1/ATM/test.java
@@ -1,24 +1,35 @@
 package ATM;
 
 import ATM.BankAccounts.BankAccount;
-import FileParsers.AccountFileReader;
-import FileParsers.AccountFileWriter;
-import FileParsers.UserFileReader;
+import FileParsers.*;
 
 import java.io.IOException;
 import java.util.Date;
 
 public class test {
     public static void main(String[] args) throws IOException {
-        AccountFileReader fileReader = new AccountFileReader("phase1/ATM/BankAccounts.txt");
-        System.out.println(fileReader.getAccounts());
-
-
-        AccountFileWriter fileWriter = new AccountFileWriter("phase1/ATM/BankAccounts2.txt",fileReader.getAccounts());
-        fileWriter.write();
-
+//        AccountFileReader fileReader5 = new AccountFileReader("phase1/ATM/BankAccounts.txt");
+//        System.out.println(fileReader5.getAccounts());
+//
+//
+//        AccountFileWriter fileWriter = new AccountFileWriter("phase1/ATM/BankAccounts2.txt",fileReader5.getAccounts());
+//        fileWriter.write();
+//
+//        AtmFileReader fileReader = new AtmFileReader("phase1/ATM/AtmInfo.txt");
+//        fileReader.read();
+//        System.out.println(fileReader.getDate());
+//
 //        UserFileReader fileReader1 = new UserFileReader("phase1/ATM/BankUsers.txt");
 //        System.out.println(fileReader1.getUsers());
+//
+//        UserFileReader fileReader2 = new UserFileReader("phase1/ATM/BankUsers.txt");
+//
+//        UserFileWriter fileWriter7 = new UserFileWriter("phase1/ATM/BankUsers1.txt",fileReader2.getUsers());
+//        fileWriter7.write();
 
+        AtmFileReader atmFileReader = new AtmFileReader("phase1/ATM/AtmInfo.txt");
+        atmFileReader.read();
+        AtmFileWriter atmFileWriter = new AtmFileWriter("phase1/ATM/info.txt",atmFileReader.getDate());
+        atmFileWriter.write();
     }
 }

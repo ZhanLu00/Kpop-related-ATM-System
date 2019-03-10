6b44a3f5da53c3dc4315396c224100ae379ed972
Roger Liu <pvtstaticvoid@gmail.com>
2019-03-09 20:16:50 -0500

    Transaction class.

--- phase1/ATM/Transaction.java
+++ phase1/ATM/Transaction.java
@@ -1,31 +1,43 @@
 package ATM;
 import ATM.BankAccounts.BankAccount;
-
 import java.util.Date;
 
-
+/**
+ * A Transaction class.
+ */
 public class Transaction {
 
     private final double AMOUNT;
+    private final BankAccount SENDER;
     private final BankAccount RECEIVER;
     private final Date DATE;
 
-    public Transaction(double amount, BankAccount receiver, Date date) {
+    public Transaction(double amount, BankAccount sender, BankAccount receiver, Date date) {
         this.AMOUNT = amount;
+        this.SENDER = sender;
         this.RECEIVER = receiver;
         this.DATE = date;
     }
 
-    /** Getters **/
-
-    public double getAMOUNT() {
+    public double getAmount() {
         return AMOUNT;
     }
 
-    public Date getDATE() {
+    public BankAccount getSender() {
+        return SENDER;
+    }
+
+    public BankAccount getReceiver() {
+        return RECEIVER;
+    }
+
+    public Date getDate() {
         return DATE;
     }
 
-    public BankAccount getReceiver() { return RECEIVER; }
+    @Override
+    public String toString() {
+        return String.format("[%s] %s -> %s ($%s)", DATE, SENDER, RECEIVER, AMOUNT);
+    }
 
 }

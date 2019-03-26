a310a7a06e2110932aa5cf77e470525562c4ea83
ahnuy <yuhan25@hotmail.com>
2019-03-25 23:23:53 -0400

    initialized welcome page

--- phase2/ATM/ATMGUI.java
+++ phase2/ATM/ATMGUI.java
@@ -1,11 +1,44 @@
 package ATM;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.NumberFormat;
 
 public class ATMGUI {
 
     NumberFormat df = NumberFormat.getCurrencyInstance(); //currency format
 
-    private JTextField welcomeText;
+    private JPanel welcomePage;
+    private JButton newUserButton;
+    private JButton returningUserButton;
+    private JLabel welcomeText;
+    private JLabel newOrReturningUser;
+
+    public ATMGUI(){
+        newUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                welcomePage.setVisible(false);
+                // JOptionPane.showMessageDialog(null, "you pressed the new user button");
+            }
+        });
+        returningUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                welcomePage.setVisible(false);
+                // JOptionPane.showMessageDialog(null, "you pressed the new returning button");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("ATM");
+        frame.setResizable(false);
+        frame.setSize(500,500);
+        frame.setContentPane(new ATMGUI().welcomePage);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.pack();
+        frame.setVisible(true);
+    }
 }

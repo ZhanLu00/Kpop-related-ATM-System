4878d6dc4e063a56fa44a03fb9ea66bb61979a55
ahnuy <yuhan25@hotmail.com>
2019-03-26 15:21:16 -0400

    add panels + (maybe) finished deposit for clients

--- phase2/ATM/ATMGUI.java
+++ phase2/ATM/ATMGUI.java
@@ -7,12 +7,20 @@ import ATM.Users.User;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.text.NumberFormat;
 
 public class ATMGUI {
 
     NumberFormat df = NumberFormat.getCurrencyInstance(); //currency format
+
+    // FIXME ahhhh
+    private AccountManager accountManager;
+    private BillManager billManager;
+    private Client client;
+
 
     Atm atm = new Atm("phase1/ATM/BankUsers.txt","phase1/ATM/BankAccounts.txt",
             "phase1/ATM/AtmInfo.txt", "phase1/ATM/alerts.txt");
@@ -35,9 +43,38 @@ public class ATMGUI {
     private JButton returnToTheMainButton;
     private JLabel enterPassword;
     private JButton createUser;
+    private JPanel clientOptions;
+    private JButton viewSummaryOfAllButton;
+    private JButton viewAccountCreationDateButton;
+    private JButton withdrawMoneyButton;
+    private JButton transferMoneyButton;
+    private JButton payABillButton;
+    private JButton depositMoneyButton;
+    private JButton viewMostRecentTransactionButton;
+    private JButton checkNetTotalButton;
+    private JButton returnToTheMainButton1;
+    private JPanel managerOptions;
+    private JButton createNewClientButton;
+    private JButton undoTransactionButton;
+    private JButton setTimeButton;
+    private JButton clearAlertsButton;
+    private JButton restockMachineButton;
+    private JButton viewAccountCreationRequestsButton;
+    private JButton showAlertsButton;
+    private JButton returnToTheMainButton2;
+    private JPanel depositOptions;
+    private JSpinner numFives;
+    private JSpinner numTens;
+    private JSpinner numTwenty;
+    private JSpinner numFifty;
+    private JFormattedTextField depositAccNum;
+    private JButton depositButton;
+    private JTextField depositAccount;
 
 
     public ATMGUI() throws IOException {
+
+        // TODO separate methods into helper methods to avoid long method code smell?
 
         newUser.addActionListener(new ActionListener() {
             @Override
@@ -65,26 +102,94 @@ public class ATMGUI {
             public void actionPerformed(ActionEvent e) {
                 User user = atm.getUser(usernameText.getText(), passwordText.getText());
                 if (user instanceof BankManager){
-                    // TODO
+                    returningUserPage.setVisible(false);
+                    managerOptions.setVisible(true);
                 }else if (user instanceof Client){
-                    // TODO
+                    returningUserPage.setVisible(false);
+                    clientOptions.setVisible(true);
+                    client = new Client(usernameText.getText(), passwordText.getText());
                 }else{
+                    usernameText.setText("");
+                    passwordText.setText("");
                     JOptionPane.showMessageDialog(null, "Incorrect username/password. " +
                             "Please try again.");
                 }
+            }
+        });
+        returnToTheMainButton1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clientOptions.setVisible(false);
+                welcomePage.setVisible(true);
+            }
+        });
+        returnToTheMainButton2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                managerOptions.setVisible(false);
+                welcomePage.setVisible(true);
+            }
+        });
+        depositMoneyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clientOptions.setVisible(false);
+                depositOptions.setVisible(true);
+            }
+        });
+        // only allow number input
+        // TODO prevent pasted text from allowing text?
+        depositAccNum.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                super.keyTyped(e);
+                char c = e.getKeyChar();
+                if (!(Character.isDigit(c) || (c == KeyEvent.VK_BACK_SPACE) || (c == KeyEvent.VK_DELETE))){
+                    e.consume();
+                }
+            }
+        });
+        // TODO -- does this work if acc num is correct
+        depositButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Object accountNum = depositAccNum.getValue();
+                if (client.accountNumbers.contains(accountNum)){
+                    int fives = (int) numFives.getValue();
+                    int tens = (int) numTens.getValue();
+                    int twenties = (int) numTwenty.getValue();
+                    int fifties = (int) numFifty.getValue();
+                    accountManager.getAccount((int)accountNum).deposit(fives*5+tens*10+twenties*20+fifties*50);
+                    billManager.deposit(fives, tens, twenties, fifties);
+                    JOptionPane.showMessageDialog(null, "Deposit complete, here is your new " +
+                            "balance: " + accountManager.getAccount((int)accountNum).getBalance());
+                }else{
+                    JOptionPane.showMessageDialog(null, "Account number does not exist. " +
+                            "Please try again.");
+                    depositAccNum.setText("");
+                }
+
             }
         });
     }
 
     public static void main(String[] args) throws IOException {
         final ATMGUI atm = new ATMGUI();
-        JFrame frame = new JFrame("ATM");
+        JFrame frame = new JFrame("ATM of your dreams");
         frame.setResizable(false);
-        frame.setSize(500, 500);
+        frame.setLocation(500,300);
+        frame.setSize(600, 500);
         frame.setContentPane(atm.root);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        // frame.pack();
         frame.setVisible(true);
         atm.welcomePage.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        numFives = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
+        numTens = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
+        numTwenty = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
+        numFifty = new JSpinner(new SpinnerNumberModel(0, 0, 100, 1));
     }
 }

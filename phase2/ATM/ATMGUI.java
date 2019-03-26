0b328c145274f2f8daae0bf8510a6419fdef53d4
ahnuy <yuhan25@hotmail.com>
2019-03-26 11:40:03 -0400

    changed layout to card so I can hide panels and stuff

--- phase2/ATM/ATMGUI.java
+++ phase2/ATM/ATMGUI.java
@@ -5,7 +5,6 @@ import ATM.Users.Client;
 import ATM.Users.User;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -19,34 +18,46 @@ public class ATMGUI {
             "phase1/ATM/AtmInfo.txt", "phase1/ATM/alerts.txt");
 
 
+    /**
+     * initialize the form (do not modify this code)
+     */
+    private JPanel root;
+    private JButton newUser;
     private JPanel welcomePage;
-    private JButton newUserButton;
-    private JButton returningUserButton;
-    private JLabel welcomeText;
-    private JLabel newOrReturningUser;
-    private JPanel panel1;
     private JPanel newUserPage;
+    private JButton returningUser;
+    private JLabel welcomeText;
     private JPanel returningUserPage;
-    private JTextField username;
-    private JPasswordField password;
+    private JTextField usernameText;
+    private JPasswordField passwordText;
     private JButton loginButton;
-    private JLabel usernameText;
-    private JLabel passwordText;
+    private JLabel enterUsername;
+    private JButton returnToTheMainButton;
+    private JLabel enterPassword;
+    private JButton createUser;
 
 
     public ATMGUI() throws IOException {
-        newUserButton.addActionListener(new ActionListener() {
+
+        newUser.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 welcomePage.setVisible(false);
                 newUserPage.setVisible(true);
             }
         });
-        returningUserButton.addActionListener(new ActionListener() {
+        returningUser.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 welcomePage.setVisible(false);
                 returningUserPage.setVisible(true);
+            }
+        });
+        returnToTheMainButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                returningUserPage.setVisible(false);
+                welcomePage.setVisible(true);
             }
         });
         loginButton.addActionListener(new ActionListener() {
@@ -57,18 +68,23 @@ public class ATMGUI {
                     // TODO
                 }else if (user instanceof Client){
                     // TODO
+                }else{
+                    JOptionPane.showMessageDialog(null, "Incorrect username/password. " +
+                            "Please try again.");
                 }
             }
         });
     }
 
     public static void main(String[] args) throws IOException {
+        final ATMGUI atm = new ATMGUI();
         JFrame frame = new JFrame("ATM");
         frame.setResizable(false);
         frame.setSize(500, 500);
-        frame.setContentPane(new ATMGUI().welcomePage);
+        frame.setContentPane(atm.root);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        atm.welcomePage.setVisible(true);
     }
 }

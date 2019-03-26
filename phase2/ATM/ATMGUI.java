02972a11489f51e599d721b3ca4960c97d83d1c7
ahnuy <yuhan25@hotmail.com>
2019-03-25 21:27:57 -0400

    initialize gui

---
+++ phase2/ATM/ATMGUI.java
@@ -0,0 +1,11 @@
+package ATM;
+
+import javax.swing.*;
+import java.text.NumberFormat;
+
+public class ATMGUI {
+
+    NumberFormat df = NumberFormat.getCurrencyInstance(); //currency format
+
+    private JTextField welcomeText;
+}

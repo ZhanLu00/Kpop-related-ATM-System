c94bd29cb78f7a5005bb7b6a9c4c7b8a66a3291a
Zhan Lu <zhanlu0523@gmail.com>
2019-03-31 13:10:46 -0400

    add plan for requestManager

--- phase2/src/ATM/Managers/RequestManager.java
+++ phase2/src/ATM/Managers/RequestManager.java
@@ -1,4 +1,20 @@
 package ATM.Managers;
 
 public class RequestManager {
+    /* Request manager
+     * Attributes
+     * * type - new user
+     *        - new account
+     * * username
+     * * account type
+     * * status - declined
+     *          - accepted
+     *          - pending
+     * Methods
+     * * setAlert(type, username, accountType)
+     * * getAlert(type, username, accountType)
+     * * getAllUserAlert()
+     * * requestExist(username) -> boolean
+     * * getStatus(type, username, accountType)
+     */
 }
